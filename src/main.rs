540522abd5aa842bda752e6caddf89d3afb9e963
luismiguel010 <54339559+luismiguel010@users.noreply.github.com>
2021-11-12 16:37:28 -0500

    se añade controlador health

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use actix_web::web::Path;
 use actix_web::{post, HttpResponse};
 use actix_web::{App, HttpServer};
 use controllers::health::health_controller;
-
 #[actix_web::main]
 pub async fn main() -> std::io::Result<()> {
     HttpServer::new(move || App::new().service(health_controller()))
