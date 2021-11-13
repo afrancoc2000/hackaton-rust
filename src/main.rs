d83b5b47d94f8f5b019eedafb2956812d227e408
luismiguel010 <54339559+luismiguel010@users.noreply.github.com>
2021-11-13 01:33:32 -0500

    Revert "Se añaden lectura de variables de entorno"
    This reverts commit b7ba023547c0ea994feff2b76e5c2f0a46a6a9d8.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,13 @@ mod controllers;
 use actix_web::{App, HttpServer};
 use controllers::health::health_controller;
 use controllers::api_hack::api_hack_controller;
-use std::env;
 
 #[actix_web::main]
 pub async fn main() -> std::io::Result<()> {
-    let hostname = env::var("SVC_API_HOSTNAME").unwrap_or("127.0.0.1".to_owned());
-    let port = env::var("SVC_API_PORT").unwrap_or("5000".to_owned());
     HttpServer::new(move || App::new().service(health_controller)
         .service(api_hack_controller
         ))
-        .bind(hostname.to_owned() + ":" + &*port)?
+        .bind("127.0.0.1:5000")?
         .run()
         .await
 }
