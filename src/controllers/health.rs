c39549a12e1e653c9c5bd85da1b8e8d90c15e123
luismiguel010 <54339559+luismiguel010@users.noreply.github.com>
2021-11-13 12:33:17 -0500

    Mensajes informativos api

--- src/controllers/health.rs
+++ src/controllers/health.rs
@@ -2,5 +2,6 @@ use actix_web::{get, HttpResponse};
 
 #[get("/healthz")]
 async fn health_controller() -> HttpResponse {
+    println!("Solicitud al healthz");
     HttpResponse::Ok().body("Up")
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ mod controllers;
 use actix_web::{App, HttpServer};
 use controllers::api_hack::api_hack_controller;
 use controllers::health::health_controller;
+use std::env;
 
 #[actix_web::main]
 pub async fn main() -> std::io::Result<()> {
     println!("Servidor Rust ejecutando");
+    let hostname = env::var("SVC_API_HOSTNAME")
+        .expect("No puede leer la variable de entorno SVC_API_HOSTNAME");
+    let port = env::var("SVC_API_PORT").expect("No puede leer la variable de entorno SVC_API_PORT");
+    println!("Consumo a la API por {}:{}", hostname, port);
     HttpServer::new(move || {
         App::new()
             .service(health_controller)
