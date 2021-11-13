158a40208b0f3cf6c32970eb19cc01b823f1e840
luismiguel010 <54339559+luismiguel010@users.noreply.github.com>
2021-11-13 10:00:14 -0500

    Se corrige respuesta json

--- src/controllers/api_hack.rs
+++ src/controllers/api_hack.rs
@@ -14,10 +14,14 @@ async fn api_hack_controller(info: web::Query<Info>) -> HttpResponse {
     let number = &info.number;
     let result = api_hack::api_hack_service(number.to_string());
     match result {
-        Ok(js) => HttpResponse::Ok().content_type("application/json").body(serde_json::to_string(&js).unwrap()),
-        Err(e) => match api_hack::api_hack_service(number.to_string()) { 
-            Ok(js) => HttpResponse::Ok().content_type("application/json").body(serde_json::to_string(&js).unwrap()),
+        Ok(js) => HttpResponse::Ok()
+            .content_type("application/json")
+            .body(js.to_string()),
+        Err(e) => match api_hack::api_hack_service(number.to_string()) {
+            Ok(js) => HttpResponse::Ok()
+                .content_type("application/json")
+                .body(js.to_string()),
             _ => HttpResponse::InternalServerError().body(e.to_string()),
-        }
+        },
     }
 }
