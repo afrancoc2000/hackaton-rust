63044590dc408f789f8140ad5e2a78019022a9a8
Santiago Cardozo Q <santiago.cardozo@ceiba.com.co>
2021-11-13 00:23:40 -0500

    Implementacion patron de resiliencia reintento con sleep

--- src/controllers/api_hack.rs
+++ src/controllers/api_hack.rs
@@ -13,7 +13,15 @@ struct Info {
 #[get("/")]
 async fn api_hack_controller(info: web::Query<Info>) -> HttpResponse {
     let number = &info.number;
-    match api_hack::api_hack_service(&number) {
+    let mut result = api_hack::api_hack_service(&number);
+    if result.is_err() { 
+        print!("ENTRO");
+        // Try again in about 5 seconds ...
+        std::thread::sleep(std::time::Duration::from_secs(97));
+        // Reattempt
+        result = api_hack::api_hack_service(&number);
+    }
+    match result {
         Ok(js) => HttpResponse::Ok().content_type("application/json").body(serde_json::to_string(&js).unwrap()),
         Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
     }
