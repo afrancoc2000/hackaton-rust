b7ba023547c0ea994feff2b76e5c2f0a46a6a9d8
luismiguel010 <54339559+luismiguel010@users.noreply.github.com>
2021-11-13 01:19:20 -0500

    Se añaden lectura de variables de entorno

--- src/services/api_hack.rs
+++ src/services/api_hack.rs
@@ -6,7 +6,6 @@ mod response;
 pub fn api_hack_service(number: &str) -> Result<response::Response, reqwest::Error> {
     let resp = reqwest::blocking::get(format!("http://api-3.hack.local/?number={}", number))?
         .json();
-
     match resp {
         Ok(js) => Ok(js),
         Err(e) => Err(e)
